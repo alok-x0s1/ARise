de4d5c546c1427e4ca61183222b6c70d6b58cd59
Alok Yadav <yalok6321@gmail.com>
2024-08-01 15:27:01 +0530

    firesbase config

--- src/conf/firebaseConf.js
+++ src/conf/firebaseConf.js
@@ -1,4 +1,10 @@
 const firebaseConfig = {
+    apikey: String(process.env.FIREBASE_API_KEY),
+    authDomain: String(process.env.FIREBASE_AUTH_DOMAIN),
+    projectId: String(process.env.FIREBASE_PROJECT_ID),
+    storageBucket: String(process.env.FIREBASE_STORAGE_BUCKET),
+    messagingSenderId: String(process.env.FIREBASE_MESSAGING_SENDER_ID),
+    appId: String(process.env.FIREBASE_APP_ID),
 };
 
 export default firebaseConfig;
