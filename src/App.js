db08ecd2a36f38846d5f09332cf5a41d6d00f013
Alok Yadav <yalok6321@gmail.com>
2024-08-02 17:11:22 +0530

    not found page and react-router config

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Contact from "./pages/Contact";
+import NotFound from "./components/NotFound";
 
 const App = () => {
 	return (
-        <div className="App">
-            <h1>Hello World!</h1>
+        <div>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+            <Footer />
         </div>
     );
 }
