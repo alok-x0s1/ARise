85ba92b5da87f714d8deaba45f6f04ebae4d0232
Alok Yadav <yalok6321@gmail.com>
2024-08-02 14:21:08 +0530

    Configure tailwind css

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import "./App.css";
-import authService from "./firebase/auth";
-import { useState } from "react";
 
-function App() {
-	const data = {
-		email: "alok@a.com",
-		password: "123456abc",
-	};
-	const createUser = async (x) => {
-		const data = await authService.createAccount(x);
-		console.log(data);
-	};
-  const getuser = async () => {
-    const user = await authService.getCurrentUser();
-    console.log(user);
-  }
+const App = () => {
 	return (
-		<div className="App">
-			<h1>Hello</h1>
-			<button onClick={() => createUser(data)}>Create User</button>
-      <button onClick={getuser}>Get User</button>
-		</div>
-	);
+        <div className="App">
+            <h1>Hello World!</h1>
+        </div>
+    );
 }
 
-export default App;
+export default App
