701456e2f3c6b942fe557b9999c6dcd3622ae1ec
Alok Yadav <yalok6321@gmail.com>
2024-08-03 14:19:17 +0530

    Setting react router dom

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Contact from "./pages/Contact";
 import NotFound from "./components/NotFound";
+import AddProduct from "./pages/AddProduct";
+import Products from "./pages/Products";
+import About from "./pages/About";
 
 const App = () => {
 	return (
@@ -16,6 +19,9 @@ const App = () => {
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/products" element={<Products />} />
+                <Route path="/add-product" element={<AddProduct />} />
                 <Route path="/contact" element={<Contact />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
