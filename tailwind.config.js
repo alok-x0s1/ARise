f7d2b7b9ac610f238bbbb0c4ba9374691ce8d6e5
Alok Yadav <yalok6321@gmail.com>
2024-08-03 14:19:00 +0530

    Tailwind config and image

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        ping: 'ping 1.5s cubic-bezier(0, 0, 0.2, 1) infinite',
+      },
+      keyframes: {
+        ping: {
+          '0%': { transform: 'scale(1)', opacity: '1' },
+          '100%': { transform: 'scale(2)', opacity: '0' },
+        },
+      },
+      delay: {
+        200: '200ms',
+        400: '400ms',
+      },
+    },
   },
   plugins: [],
 }
