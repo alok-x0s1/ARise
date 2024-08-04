3873d7bb99e3eabb8c138b698489a624b8e54a27
Alok Yadav <yalok6321@gmail.com>
2024-08-04 09:06:05 +0530

    react redux and react router dom and tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      animation: {
-        ping: 'ping 1.5s cubic-bezier(0, 0, 0.2, 1) infinite',
-      },
-      keyframes: {
-        ping: {
-          '0%': { transform: 'scale(1)', opacity: '1' },
-          '100%': { transform: 'scale(2)', opacity: '0' },
-        },
-      },
-      delay: {
-        200: '200ms',
-        400: '400ms',
-      },
-    },
-  },
-  plugins: [],
-}
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			animation: {
+				ping: "ping 1.5s cubic-bezier(0, 0, 0.2, 1) infinite",
+				"reduce-width": "reduce-width 5s linear forwards",
+			},
+			keyframes: {
+				ping: {
+					"0%": { transform: "scale(1)", opacity: "1" },
+					"100%": { transform: "scale(2)", opacity: "0" },
+				},
+				"reduce-width": {
+					"0%": { width: "100%" },
+					"100%": { width: "0%" },
+				},
+			},
+			delay: {
+				200: "200ms",
+				400: "400ms",
+			},
+		},
+	},
+	plugins: [],
+};
