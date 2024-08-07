c029d84effd875fc38ab95db4cd255223e6eb628
Alok Yadav <yalok6321@gmail.com>
2024-08-07 11:07:54 +0530

    config of dotenv

--- functions/index.js
+++ functions/index.js
@@ -8,12 +8,12 @@
  */
 
 // The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
-const { onRequest } = require("firebase-functions/v2/https");
+const {onRequest} = require("firebase-functions/v2/https");
 // const { onDocumentCreated } = require("firebase-functions/v2/firestore");
 // const logger = require("firebase-functions");
 
 // The Firebase Admin SDK to access Firestore.
-const { initializeApp } = require("firebase-admin/app");
+const {initializeApp} = require("firebase-admin/app");
 // const { getFirestore } = require("firebase-admin/firestore");
 
 initializeApp();
@@ -43,16 +43,15 @@ app.use(express.json());
 app.get("/", (request, response) => response.status(200).send("hello world"));
 
 app.post("/payments/create", async (request, response) => {
-	const total = request.query.total;
+  const total = request.query.total;
 
-	console.log("Payment Request Received BOOM!!! for this amount >>> ", total);
+  console.log("Payment Request Received BOOM!!! for this amount >>> ", total);
 
-	const paymentIntent = await stripe.paymentIntents.create({
-		amount: total,
-		currency: "usd",
-	});
-
-	response.status(201).send({ clientSecret: paymentIntent.client_secret });
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total,
+    currency: "usd",
+  });
+  response.status(201).send({clientSecret: paymentIntent.client_secret});
 });
 
 // Listen command
