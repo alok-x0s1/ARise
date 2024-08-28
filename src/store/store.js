d3254d6002c7f3588043f19b20958156e1ad2994
Alok Yadav <yalok6321@gmail.com>
2024-08-28 19:06:48 +0530

    config of redux persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,28 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
 import cartReducer from "../features/cartSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+const persistConfig = {
+	key: "root",
+	storage,
+};
+
+const rootReducer = {
+	auth: authReducer,
+	cart: cartReducer,
+};
+
+const persistedReducer = persistReducer(
+	persistConfig,
+	combineReducers(rootReducer)
+);
 
 const store = configureStore({
-	reducer: {
-		auth: authReducer,
-		cart: cartReducer,
-	},
+	reducer: persistedReducer,
 });
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
