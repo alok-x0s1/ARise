e2228ce3586cfc3ad1cc61276becb8570fdaea0d
Alok Yadav <yalok6321@gmail.com>
2024-09-04 17:17:27 +0530

    Config and helper functions

---
+++ src/pages/index.js
@@ -0,0 +1,27 @@
+import About from "./About";
+import AddProduct from "./AddProduct";
+import Cart from "./Cart";
+import Checkout from "./Checkout";
+import Contact from "./Contact";
+import FeaturedProductPage from "./FeaturedProductPage";
+import Home from "./Home";
+import Login from "./Login";
+import Orders from "./Orders";
+import Products from "./Products";
+import Signup from "./Signup";
+import SingleProduct from "./SingleProduct";
+
+export {
+	About,
+	AddProduct,
+	Cart,
+	Checkout,
+	Contact,
+	FeaturedProductPage,
+	Home,
+	Login,
+	Orders,
+	Products,
+	Signup,
+	SingleProduct,
+};
