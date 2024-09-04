300cc120c34b4b3daef54aa8963fc52cf3beca34
Alok Yadav <yalok6321@gmail.com>
2024-09-04 17:16:25 +0530

    Added a store for customization

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
 import cartReducer from "../features/cartSlice";
+import customizerReducer from "../features/customizerSlice";
 import storage from "redux-persist/lib/storage";
 import { persistReducer, persistStore } from "redux-persist";
 
@@ -12,6 +13,7 @@ const persistConfig = {
 const rootReducer = {
 	auth: authReducer,
 	cart: cartReducer,
+	customizer: customizerReducer,
 };
 
 const persistedReducer = persistReducer(
